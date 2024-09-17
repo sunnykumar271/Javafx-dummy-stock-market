0ada568b102e3b7c4fcf8c9d339ca1e5639d9204
Anshuman <sanshuman4422@gmail.com>
2024-09-17 17:53:18 +0530

    added mainpanel

---
+++ Bear&Bullz1/BearBullz/src/main/java/com/mycompany/bearbullz/mainpanel.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package com.mycompany.bearbullz;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author Anshu
+ */
+public class mainpanel extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        
+
+        Parent root = FXMLLoader.load(getClass().getResource("mainpanel.fxml"));
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+         scene.getStylesheets().add(getClass().getResource("Mainpanel.css").toExternalForm());
+        primaryStage.setFullScreen(true);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
