fd408ade720ba6abbaf89d730ac48a493d4634c4
sunnykumar271 <kushwasunny@gmail.com>
2024-09-20 11:26:26 +0530

    Sunny Conflict

--- Bear&Bullz1/BearBullz/src/main/java/com/mycompany/bearbullz/AboutUs.java
+++ Bear&Bullz1/BearBullz/src/main/java/com/mycompany/bearbullz/AboutUs.java
@@ -15,6 +15,7 @@ public class AboutUs extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(AboutUs.class.getResource("about_us.fxml"));
         GridPane root = fxmlLoader.load();
         Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("AboutUs.css").toExternalForm());
         stage.setTitle("About Us");
         stage.setScene(scene);
         stage.show();
